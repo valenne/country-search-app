20d69f74deb251d6cfa47b117353016488bbdf98
Nelson Venegas <nelson.venegasg@gmail.com>
2023-10-12 16:29:19 -0300

    fix: modified files for a good deploy

--- src/hooks/useFetchCountries.ts
+++ src/hooks/useFetchCountries.ts
@@ -1,7 +1,11 @@
+import { API_URL } from '../config/config-url-api';
+import { CountriesProps } from '../types/countries-types';
+
 export const useFetchCountries = async () => {
+	console.log('charmander');
+
 	try {
-		// const response = await fetch(API_URL.allCountries());
-		const response = await fetch('http://localhost:3000/api/countries', {
+		const response = await fetch(API_URL.allCountries(), {
 			method: 'GET',
 			headers: {
 				'Content-type': 'application/json'
@@ -12,8 +16,46 @@ export const useFetchCountries = async () => {
 
 		const data = await response.json();
 
-		return data;
+		const customDataReturn: CountriesProps & {
+			id: string;
+		} = data.map((country: CountriesProps) => {
+			return {
+				id: crypto.randomUUID(),
+				name: {
+					common: country.name.common
+				},
+				population: country.population,
+				capital: country.capital ?? 'n/a',
+				flags: {
+					png: country.flags.png,
+					altImg: country.flags.alt ?? `${country.name.common} flag`
+				},
+				region: country.region
+			};
+		});
+
+		return customDataReturn;
 	} catch (e) {
-		throw new Error('Fetching data error');
+		throw new Error('api not response');
 	}
 };
+
+// export const useFetchCountries = async () => {
+// 	try {
+// 		// const response = await fetch(API_URL.allCountries());
+// 		const response = await fetch('http://localhost:3000/api/countries', {
+// 			method: 'GET',
+// 			headers: {
+// 				'Content-type': 'application/json'
+// 			}
+// 		});
+
+// 		if (!response.ok || response.status === 404) return;
+
+// 		const data = await response.json();
+
+// 		return data;
+// 	} catch (e) {
+// 		throw new Error('Fetching data error');
+// 	}
+// };
--- src/hooks/useFetchCountry.ts
+++ src/hooks/useFetchCountry.ts
@@ -1,20 +1,79 @@
+import { API_URL } from '../config/config-url-api';
+import returnFullNamesBorder from '../utils/border-fullnames-countries';
+
+// export const useFetchCountry = async (name: string, capital: string) => {
+// 	try {
+// 		const response = await fetch(`http://localhost:3000/api/country/${name}`, {
+// 			method: 'GET',
+// 			headers: {
+// 				'Content-type': 'application/json',
+// 				capital
+// 			},
+// 			next: { revalidate: 60 }
+// 		});
+// 		``;
+// 		if (!response.ok || response.status === 404) return;
+
+// 		const data = await response.json();
+
+// 		return data;
+// 	} catch (e) {
+// 		throw new Error('Fetching data error');
+// 	}
+// };
+
 export const useFetchCountry = async (name: string, capital: string) => {
+	console.log('charizard');
 	try {
-		const response = await fetch(`http://localhost:3000/api/country/${name}`, {
-			method: 'GET',
-			headers: {
-				'Content-type': 'application/json',
-				capital
-			},
-			next: { revalidate: 60 }
-		});
-		``;
+		const response = await fetch(API_URL.countriesByName(name));
+
 		if (!response.ok || response.status === 404) return;
 
 		const data = await response.json();
 
-		return data;
-	} catch (e) {
-		throw new Error('Fetching data error');
+		// grab country borders
+		const [borders] = data.map((coun: any) => {
+			if (coun.borders === undefined) return ['N/A'];
+			return coun.borders;
+		});
+
+		const fullNameBorders = await returnFullNamesBorder(borders);
+		console.log(borders);
+
+		if (!fullNameBorders || fullNameBorders.length === 0) return;
+
+		const countryDataReturn = data.map((country: any) => {
+			for (let i = 0; i < country.capital.length; i++) {
+				if (country.capital[i].toLocaleLowerCase() === capital?.toLocaleLowerCase()) {
+					const arrLang = Object.values(country.languages);
+
+					const [arrCurrencies] = Object.values(country.currencies);
+
+					return {
+						name: {
+							common: country.name.common,
+							official: country.name.official
+						},
+						flags: {
+							png: country.flags.png,
+							alt: country.flags.alt
+						},
+						population: country.population,
+						region: country.region,
+						subregion: country.subregion,
+						capital: country.capital,
+						tld: country.tld,
+						languages: arrLang,
+						borders: country.borders,
+						fullNamesBorder: fullNameBorders && fullNameBorders,
+						currencies: arrCurrencies
+					};
+				}
+			}
+		});
+
+		return countryDataReturn;
+	} catch (err) {
+		console.log(err);
 	}
 };
